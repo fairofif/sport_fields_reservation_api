971c882f3530882c6bb9b765142e84a43b7a1ce5
fairofif <fairofif@gmail.com>
2024-02-13 00:18:41 +0700

    update new dev env

--- db_config.py
+++ db_config.py
@@ -6,8 +6,8 @@ import os
 
 
 mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = os.getenv("DB_USER")
-app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv("DB_PASSWORD")
-app.config['MYSQL_DATABASE_DB'] = os.getenv("DB_NAME")
-app.config['MYSQL_DATABASE_HOST'] = os.getenv("DB_HOST")
+app.config['MYSQL_DATABASE_USER'] = os.getenv("DB_USER_")
+app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv("DB_PASSWORD_")
+app.config['MYSQL_DATABASE_DB'] = os.getenv("DB_NAME_")
+app.config['MYSQL_DATABASE_HOST'] = os.getenv("DB_HOST_")
 mysql.init_app(app)
