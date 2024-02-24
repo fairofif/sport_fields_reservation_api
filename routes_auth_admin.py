b1614244528d56c5fe2eceb9613d07782df4efc9
fairofif <fairofif@gmail.com>
2024-02-25 01:29:09 +0700

    admin auth implemented, change on null return values from string into None

--- routes_auth_admin.py
+++ routes_auth_admin.py
@@ -4,28 +4,27 @@ from flask import (
     request
 )
 from db_config import mysql
-from token_generator import newPlayerToken
+from token_generator import newAdminToken
 from dotenv import load_dotenv
 load_dotenv()
 import os
 
-def player_auth_configure_routes(app):
-    # ============================= ROUTES ==================================== #
-
-    @app.route('/player/auth/register', methods=['POST'])
-    def player_register():
-        """route for register new player user"""
+def admin_auth_configure_routes(app):
+    @app.route('/admin/auth/register', methods=['POST'])
+    def admin_register():
+        """route for register new admin user"""
         data = request.json
         username = data['username']
         password = data['password']
         name = data['name']
+        phone = data['phone']
         ava_url = os.getenv("DEFAULT_AVA_PATH")
-        query = 'SELECT username FROM Player WHERE username = "'+str(username)+'"'
+        query = 'SELECT username FROM Admin WHERE username = "'+str(username)+'"'
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
         cursor.execute(query)
         if cursor.rowcount == 0:
-            query_insert = "INSERT INTO Player VALUES ('"+username+"', '"+password+"', '"+name+"', '"+ava_url+"', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP())"
+            query_insert = "INSERT INTO Admin VALUES ('"+username+"', '"+name+"', '"+password+"', '"+phone+"', '"+ava_url+"', CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP())"
             cursor.execute(query_insert)
             response = {
                 "message": "Register Success",
@@ -42,13 +41,13 @@ def player_auth_configure_routes(app):
 
         return jsonify(response)
 
-    @app.route('/player/auth/login', methods=['POST'])
-    def player_login():
+    @app.route('/admin/auth/login', methods=['POST'])
+    def admin_login():
         data = request.json
         username = data['username']
         password = data['password']
         virtual_device_id = data['virtual_device_id']
-        query = "SELECT username FROM Player WHERE username = '"+username+"'"
+        query = "SELECT username FROM Admin WHERE username = '"+username+"'"
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
         cursor.execute(query)
@@ -60,13 +59,14 @@ def player_auth_configure_routes(app):
                 "token": None,
                 "username": None,
                 "name": None,
-                "ava_url": None
+                "ava_url": None,
+                "phone": None
             }
             cursor.close()
             conn.close()
         else:
             ## username exist
-            query = "SELECT * FROM Player WHERE username = '"+username+"' AND password = '"+password+"'"
+            query = "SELECT * FROM Admin WHERE username = '"+username+"' AND password = '"+password+"'"
             cursor.execute(query)
             if cursor.rowcount == 0:
                 ## password not valid
@@ -82,19 +82,19 @@ def player_auth_configure_routes(app):
                 conn.close()
             else:
                 ## login valid
-                token = newPlayerToken()
+                token = newAdminToken()
 
                 ## delete existing token within device ID
-                query = "DELETE FROM Player_Login_Token WHERE Virtual_Device_ID_id='"+virtual_device_id+"'"
+                query = "DELETE FROM Admin_Login_Token WHERE Virtual_Device_ID_id='"+virtual_device_id+"'"
                 cursor.execute(query)
                 conn.commit()
 
-                query = "INSERT INTO Player_Login_Token VALUES ('"+token+"', '"+username+"', CURRENT_TIMESTAMP(), '"+virtual_device_id+"')"
+                query = "INSERT INTO Admin_Login_Token VALUES ('"+token+"', '"+username+"', CURRENT_TIMESTAMP(), '"+virtual_device_id+"')"
 
                 cursor.execute(query)
                 conn.commit()
 
-                query = "SELECT * FROM Player WHERE username = '"+username+"'"
+                query = "SELECT * FROM Admin WHERE username = '"+username+"'"
                 cursor.execute(query)
                 read_row = cursor.fetchone()
                 cursor.close()
@@ -106,15 +106,16 @@ def player_auth_configure_routes(app):
                     "token": token,
                     "username": username,
                     "name": read_row['name'],
-                    "ava_url": read_row['ava_url']
+                    "ava_url": read_row['ava_url'],
+                    "phone": read_row['phone']
                 }
         return jsonify(response)
 
-    @app.route('/player/auth/relogin', methods=['GET'])
-    def player_relogin():
+    @app.route('/admin/auth/relogin', methods=['GET'])
+    def admin_relogin():
         header = request.headers
         token = header['token']
-        query = "SELECT token FROM Player_Login_Token WHERE token = '"+token+"'"
+        query = "SELECT token FROM Admin_Login_Token WHERE token = '"+token+"'"
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
         cursor.execute(query)
@@ -124,13 +125,14 @@ def player_auth_configure_routes(app):
                 "username": None,
                 "name": None,
                 "ava_url": None,
+                "phone": None,
                 "message": "Token is already expired"
             }
         else:
-            query = ("SELECT Player.username, Player.name, Player.ava_url, Player_Login_Token.token FROM Player"
-                     + " INNER JOIN Player_Login_Token ON"
-                     + " (Player_Login_Token.Player_username = Player.username)"
-                     + " WHERE Player_Login_Token.token = '"+token+"'")
+            query = ("SELECT Admin.username, Admin.name, Admin.ava_url, Admin.phone, Admin_Login_Token.token FROM Admin"
+                     + " INNER JOIN Admin_Login_Token ON"
+                     + " (Admin_Login_Token.Admin_username = Admin.username)"
+                     + " WHERE Admin_Login_Token.token = '"+token+"'")
             cursor.execute(query)
             read_row = cursor.fetchone()
             response = {
@@ -138,6 +140,7 @@ def player_auth_configure_routes(app):
                 "username": read_row['username'],
                 "name": read_row['name'],
                 "ava_url": read_row['ava_url'],
+                "phone": read_row['phone'],
                 "message": "Token is valid, relogin successfully"
             }
         cursor.close()
@@ -145,11 +148,11 @@ def player_auth_configure_routes(app):
 
         return jsonify(response)
 
-    @app.route('/player/auth/logout', methods=['DELETE'])
-    def player_logout():
+    @app.route('/admin/auth/logout', methods=['DELETE'])
+    def admin_logout():
         header = request.headers
         token = header['token']
-        query = "DELETE FROM Player_Login_Token WHERE token = '"+token+"'"
+        query = "DELETE FROM Admin_Login_Token WHERE token = '"+token+"'"
         conn = mysql.connect()
         cursor = conn.cursor(pymysql.cursors.DictCursor)
         cursor.execute(query)
--- test_auth_admin.py
+++ test_auth_admin.py
@@ -4,15 +4,13 @@ from app import app
 from dotenv import load_dotenv
 load_dotenv()
 import os
-from token_generator import newPlayerToken
+from token_generator import newAdminToken
 from virtual_device_id_generator import newVirtualDeviceID
-
-# ============================= Local Function ==============================
 
 def insert_unittest_user():
     ava_url = os.getenv("DEFAULT_AVA_PATH")
 
-    query = 'INSERT INTO Player VALUES("unittest", "unittest", "Unit Test", "'+ava_url+'", CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP())'
+    query = 'INSERT INTO Admin VALUES("unittest", "Unit Test", "unittest", "unittest", "'+ava_url+'", CURRENT_TIMESTAMP(), CURRENT_TIMESTAMP())'
     conn = mysql.connect()
     cursor = conn.cursor(pymysql.cursors.DictCursor)
     cursor.execute(query)
@@ -21,7 +19,7 @@ def insert_unittest_user():
     conn.close()
 
 def delete_unittest_user():
-    query = 'DELETE FROM Player WHERE username = "unittest"'
+    query = 'DELETE FROM Admin WHERE username = "unittest"'
     conn = mysql.connect()
     cursor = conn.cursor(pymysql.cursors.DictCursor)
     cursor.execute(query)
@@ -30,7 +28,7 @@ def delete_unittest_user():
     conn.close()
 
 def delete_unittest_token(token):
-    query = "DELETE FROM Player_Login_Token WHERE token = '"+token+"'"
+    query = "DELETE FROM Admin_Login_Token WHERE token = '"+token+"'"
     conn = mysql.connect()
     cursor = conn.cursor(pymysql.cursors.DictCursor)
     cursor.execute(query)
@@ -39,7 +37,7 @@ def delete_unittest_token(token):
     conn.close()
 
 def insert_unittest_token(token, device_id):
-    query = "INSERT INTO Player_Login_Token VALUES ('"+token+"', 'unittest', CURRENT_TIMESTAMP(), '"+device_id+"')"
+    query = "INSERT INTO Admin_Login_Token VALUES ('"+token+"', 'unittest', CURRENT_TIMESTAMP(), '"+device_id+"')"
     conn = mysql.connect()
     cursor = conn.cursor(pymysql.cursors.DictCursor)
     cursor.execute(query)
@@ -65,22 +63,23 @@ def delete_unittest_device(device_id):
     cursor.close()
     conn.close()
 
-# ============================= UNIT TEST ===================================
+### ============================ UNIT TEST ================================= ###
 
-def test_auth_player_register():
-    """Testing register player success"""
+def test_auth_admin_register():
+    """Testing register admin success"""
     client = app.test_client()
-    url = '/player/auth/register'
+    url = '/admin/auth/register'
 
     mock_request_body = {
         "username": "unittest",
         "password": "unittest",
-        "name": "Unit Test"
+        "name": "Unit Test",
+        "phone": "08123456789"
     }
 
     response = client.post(url, json=mock_request_body)
 
-    query = 'DELETE FROM Player WHERE username = "'+mock_request_body["username"]+'"'
+    query = 'DELETE FROM Admin WHERE username = "'+mock_request_body["username"]+'"'
     conn = mysql.connect()
     cursor = conn.cursor(pymysql.cursors.DictCursor)
     cursor.execute(query)
@@ -91,17 +90,18 @@ def test_auth_player_register():
     assert response.status_code == 200
     assert response.get_json()['register_status'] == True
 
-def test_auth_player_register_failed():
-    """Testing register player failed"""
+def test_auth_admin_register_failed():
+    """Testing register admin failed"""
     client = app.test_client()
-    url = '/player/auth/register'
+    url = '/admin/auth/register'
 
     insert_unittest_user()
 
     mock_request_body = {
         "username": "unittest",
         "password": "unittest",
-        "name": "Unit Test"
+        "name": "Unit Test",
+        "phone": "08123456789"
     }
 
     response = client.post(url, json=mock_request_body)
@@ -111,10 +111,10 @@ def test_auth_player_register_failed():
     assert response.status_code == 200
     assert response.get_json()['register_status'] == False
 
-def test_auth_player_login_success():
-    """Testing login player success"""
+def test_auth_admin_login_success():
+    """Testing login admin success"""
     client = app.test_client()
-    url = '/player/auth/login'
+    url = '/admin/auth/login'
 
     insert_unittest_user()
     device_id = newVirtualDeviceID()
@@ -134,10 +134,10 @@ def test_auth_player_login_success():
     assert response.status_code == 200
     assert response.get_json()['login_status'] == True
 
-def test_auth_player_login_username_not_found():
-    """Testing login player username not found"""
+def test_auth_admin_login_username_not_found():
+    """Testing login admin username not found"""
     client = app.test_client()
-    url = '/player/auth/login'
+    url = '/admin/auth/login'
 
     device_id = newVirtualDeviceID()
     insert_unittest_device(device_id)
@@ -157,10 +157,10 @@ def test_auth_player_login_username_not_found():
     assert response.get_json()['login_status'] == False
     assert response.get_json()['message'] == "Username not found"
 
-def test_auth_player_login_password_wrong():
-    """Testing login player password wrong"""
+def test_auth_admin_login_password_wrong():
+    """Testing login admin password wrong"""
     client = app.test_client()
-    url = '/player/auth/login'
+    url = '/admin/auth/login'
 
     insert_unittest_user()
     device_id = newVirtualDeviceID()
@@ -181,12 +181,12 @@ def test_auth_player_login_password_wrong():
     assert response.get_json()['login_status'] == False
     assert response.get_json()['message'] == "Password doesn't match"
 
-def test_auth_player_relogin_token_valid():
+def test_auth_admin_relogin_token_valid():
     """Testing relogin with valid token"""
     client = app.test_client()
-    url = '/player/auth/relogin'
+    url = '/admin/auth/relogin'
 
-    token = newPlayerToken()
+    token = newAdminToken()
     insert_unittest_user()
     device_id = newVirtualDeviceID()
     insert_unittest_device(device_id)
@@ -204,18 +204,18 @@ def test_auth_player_relogin_token_valid():
     assert response.get_json()['message'] == "Token is valid, relogin successfully"
     assert response.get_json()['username'] == "unittest"
 
-def test_auth_player_relogin_token_invalid():
+def test_auth_admin_relogin_token_invalid():
     """Testing relogin with invalid token"""
     client = app.test_client()
-    url = '/player/auth/relogin'
+    url = '/admin/auth/relogin'
 
-    token = newPlayerToken()
+    token = newAdminToken()
     insert_unittest_user()
     device_id = newVirtualDeviceID()
     insert_unittest_device(device_id)
     insert_unittest_token(token, device_id)
     header = {
-        "token": newPlayerToken()
+        "token": newAdminToken()
     }
     response = client.get(url, headers=header)
 
@@ -227,15 +227,15 @@ def test_auth_player_relogin_token_invalid():
     assert response.get_json()['message'] == "Token is already expired"
     assert response.get_json()['username'] == None
 
-def test_auth_player_logout():
-    """Testing logout player"""
+def test_auth_admin_logout():
+    """Testing logout admin"""
     client = app.test_client()
-    url = '/player/auth/logout'
+    url = '/admin/auth/logout'
 
     device_id = newVirtualDeviceID()
     insert_unittest_device(device_id)
     insert_unittest_user()
-    token = newPlayerToken()
+    token = newAdminToken()
     insert_unittest_token(token, device_id)
 
     header = {
