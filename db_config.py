b4489eb85731d4f128fe483583656a49c32897ad
Rofif Fairuz Hawary <55540799+fairofif@users.noreply.github.com>
2024-03-02 04:47:36 +0700

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -11,5 +11,5 @@ app.config['MYSQL_DATABASE_PASSWORD'] = os.getenv("DB_PASSWORD_")
 app.config['MYSQL_DATABASE_DB'] = os.getenv("DB_NAME_")
 app.config['MYSQL_DATABASE_HOST'] = os.getenv("DB_HOST_")
 app.config['MYSQL_DATABASE_PORT'] = 3306
-app.config['MYSQL_DATABASE_SOCKET'] = None
+app.config['MYSQL_DATABASE_SOCKET'] = "/tmp/mysql.sock"
 mysql.init_app(app)
